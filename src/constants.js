02841b811e1008b6fbd66e78a8c9137dc58094ea
Tony Chen <66529372+tony9321@users.noreply.github.com>
2024-05-24 14:54:07 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -16,6 +16,6 @@ export const dialogueData = {
   Contact me at tonychen9321@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about my projects <a href="https://tony9321.github.io/Tony-s-Personal-Portfolio/" target="_blank">here</a>.`,
-  library: `Diary: I met a cute girl on the dating app right before i make this game.`,
+  library: `Diary: I met a girl on the dating app and inspired me to make this web app, we getting boba today!🦛👻.`,
   exit: `If you want to exit Tony's portfolio, just close the tab.`,
 };
