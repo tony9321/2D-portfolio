c6d212737e4b0c30dfb7081d999b5648bee3fb8d
Tony Chen <66529372+tony9321@users.noreply.github.com>
2024-08-19 13:42:34 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,21 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work as a full stack intern developer these days.
-    I've made a couple of apps, webs. I also like algorithm and data structures. Anyway regardless of the language, I just like programming.
+  pc: `This is my PC. I work as a full-stack intern developer these days.
+    I've made a couple of apps, and websites. I also like algorithms and data structures. Anyway regardless of the language, I like programming.
     Here is my <a href="https://github.com/tony9321" target="_blank">Github</a>!`,
   "cs-degree": `This is my Linkedin. <a href="https://www.linkedin.com/in/tony-chen-304529190/" target="_blank">my linkedin</a>`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
   I also make tutorials on YouTube. Go sub to <a href="https://www.youtube.com/channel/UCk_2B4kuttQPfIijiWpQL4Q" target="_blank">my channel</a>! (If you like the content)
   `,
-  tv: `That's my TV. I've been watching youtubers a lot recently like :
+  tv: `That's my TV. I've been watching YouTubers a lot recently like :
    <a href="https://www.youtube.com/@casey" target="_blank">Casey Neistat</a>, <a href="https://www.youtube.com/@CSJackie" target="_blank">CS Jackie</a>,
   <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp</a> (sometimes) and <a href="https://www.youtube.com/@CNN10" target="_blank">CNN10</a>!`,
   bed: `This where I sleep and do most of my tasks`,
   resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/drive/u/0/folders/11WXV8GeiAERXIHyxaaIVqyr3HdaiGaeh" target="_blank">Check it out?</a>
   Contact me at tonychen9321@gmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  projects: `Info about this portfolio: It's made with the Kaboom.js library, a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about my projects <a href="https://tony9321.github.io/Tony-s-Personal-Portfolio/" target="_blank">here</a>.`,
-  library: `Diary: I met someone online and inspired me to make this web app, we getting boba today!🦛👻.`,
+  library: `Diary: You tell me what we did today!🦛👻.`,
   exit: `If you want to exit Tony's portfolio, just close the tab.`,
 };
